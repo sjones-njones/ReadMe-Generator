d757f6c09ed8ffd84c16184e2e3d544cf0df1c69
stephanie jones <sjones.njones@gmail.com>
2023-07-29 00:45:59 -0500

    added functions

---
+++ index.js
@@ -0,0 +1,55 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+
+// TODO: Include packages needed for this application
+// TODO: Create an array of questions for user input
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Explain how to install your project.',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Give a description of your project.',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'How should this project be used?',
+  },
+  {
+    type: 'input',
+    name: 'contributers',
+    message: 'Who were the contributers to your project?',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Which license would you like to use?',
+    choices: ['N/A', 'MIT License', 'GNU General Public License v3.0', 'Apache Licence 2.0', 'Mozilla Public License 2.0', 'The Unlicense']
+  }
+];
+
+// // TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => err ? console.log(err) : console.log("Sucessfully created README.md!"));
+}
+// // TODO: Create a function to initialize app
+function init() { 
+inquirer
+  .prompt(questions)
+  .then((answers) => {
+    const readMeContent = generateMarkdown(answers);
+    writeToFile('./output/README.md', readMeContent);
+  });
+}
+// // Function call to initialize app
+init();
---
+++ output/README.md
@@ -0,0 +1,21 @@
+# asdf	
+ 
+[![GNU General Public License v3.0](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+
+dfasdf
+
+## Installation
+
+adfsd
+
+## Usage
+
+adfads
+
+## Contributing
+
+adfas
+
+## License
+
+GNU General Public License v3.0
